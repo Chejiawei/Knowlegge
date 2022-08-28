d2818b91e223b7ee7d5eca2a72f4b91e7b2cb292
CheJW <chejiawei1029@outlook.com>
2022-08-28 18:23:38 +0800

    项目创建

---
+++ src/main/java/com/chejw/top/ChejwApplication.java
@@ -0,0 +1,13 @@
+package com.chejw.top;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChejwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChejwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chejw/top/ChejwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chejw.top;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChejwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
